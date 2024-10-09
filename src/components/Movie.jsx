6f176cdf436b0e60ec9be5b755bdbe5b404e391c
Thomas Basham <Bashamtg@gmail.com>
2024-10-09 13:23:26 -0700

    updated movie component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import React from "react";
-
+import LoadingSpinner from "./LoadingSpinner";
 // Fetch movie details based on the movie ID
 const fetchMovie = async (id) => {
   const { data } = await axios.get(
@@ -12,22 +12,12 @@ const fetchMovie = async (id) => {
   return data.find((movie) => movie.id == id); // Find the movie by ID
 };
 
-// Fetch movie thumbnail from OMDb API
-const fetchThumbnail = async (title, year) => {
-  const { data } = await axios.get(
-    `https://www.omdbapi.com/?s=${title.replaceAll(" ", "_")}&apikey=${
-      process.env.NEXT_PUBLIC_OMDB_API_KEY
-    }`
-  );
-  return data?.Search[0];
-};
-
 // Fetch the average rating for the movie
 const fetchAvgRating = async (id) => {
   const { data } = await axios.get(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/movies/${id}/average-rating`
   );
-  return data || 0; // Assuming data returns the average rating
+  return data || { averageRating: 0, amountOfRatings: 0 }; // Default rating if no data
 };
 
 export default function Movie({ id }) {
@@ -40,29 +30,31 @@ export default function Movie({ id }) {
   } = useQuery({
     queryKey: ["movie", id],
     queryFn: () => fetchMovie(id),
-    staleTime: 5 * 60 * 1000, // Data is fresh for 5 minutes
-    cacheTime: 10 * 60 * 1000, // Data stays in the cache for 10 minutes
-  });
-
-  // Fetch movie thumbnail
-  const { data: thumbnail, isLoading: isThumbnailLoading } = useQuery({
-    queryKey: ["thumbnail", movieData?.title, movieData?.release_year],
-    queryFn: () => fetchThumbnail(movieData?.title, movieData?.release_year),
-    enabled: !!movieData, // Only fetch thumbnail if movie data is available
+    staleTime: 5 * 60 * 1000,
+    cacheTime: 10 * 60 * 1000,
   });
 
   // Fetch the average rating for the movie
-  const { data: avgRating = 0, isLoading: isAvgRatingLoading } = useQuery({
+  const { data: avgRating, isLoading: isAvgRatingLoading } = useQuery({
     queryKey: ["avgRating", id],
     queryFn: () => fetchAvgRating(id),
-    enabled: !!movieData, // Only fetch average rating if movie data is available
+    enabled: !!movieData,
+    staleTime: 5 * 60 * 1000,
+    cacheTime: 10 * 60 * 1000,
   });
 
-  if (isMovieLoading) return <div>Loading...</div>;
+  if (isMovieLoading) return <LoadingSpinner />;
   if (isMovieError) return <div>Error... {movieError.message}</div>;
 
   return (
-    <div className="relative max-w-4xl p-8 mx-auto bg-white rounded-lg shadow-md">
+    <div
+      className="relative max-w-4xl p-8 mx-auto bg-white rounded-lg shadow-md"
+      // style={{
+      //   backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.backdrop_path})`,
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      // }}
+    >
       {/* Display the average rating in the top-right corner */}
       {!isAvgRatingLoading && (
         <div className="absolute p-2 text-center text-white bg-yellow-500 rounded-md shadow-lg top-4 right-4">
@@ -71,33 +63,24 @@ export default function Movie({ id }) {
           </h2>
           <div className="mt-2">
             <span className="text-4xl font-extrabold">
-              {avgRating.averageRating?.toFixed(1)}
+              {avgRating?.averageRating?.toFixed(1)}
             </span>
             <span className="text-sm font-medium">/ 5</span>
           </div>
           <p className="mt-1 text-xs text-gray-100">
-            {avgRating.amountOfRatings} ratings
+            {avgRating?.amountOfRatings} ratings
           </p>
         </div>
       )}
 
       <div className="flex flex-col items-center md:flex-row md:items-start">
-        {/* Display the thumbnail if available */}
-        {isThumbnailLoading ? (
-          <div className="w-48 bg-gray-200 rounded-lg h-72 animate-pulse" />
-        ) : thumbnail ? (
-          <Image
-            src={thumbnail.Poster}
-            alt={movieData?.title}
-            width={250}
-            height={375}
-            className="rounded-lg shadow-lg"
-          />
-        ) : (
-          <div className="flex items-center justify-center w-48 bg-gray-200 rounded-lg h-72">
-            <span className="text-sm text-gray-500">No Image Available</span>
-          </div>
-        )}
+        <Image
+          src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+          alt={movieData.title}
+          width={200}
+          height={300}
+          className="rounded-lg shadow-lg"
+        />
 
         <div className="w-full mt-6 md:ml-8 md:mt-0">
           {/* Movie Title */}
@@ -112,7 +95,7 @@ export default function Movie({ id }) {
 
           {/* Movie Description */}
           <p className="text-base leading-relaxed text-gray-700">
-            {movieData?.description}
+            {movieData?.overview}
           </p>
         </div>
       </div>
